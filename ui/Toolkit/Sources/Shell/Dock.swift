6a78070907191bdb43ede21535a7d019bddca7d9
Nick Hagi <hagiionutnichi@gmail.com>
2026-09-20 00:36:38 +0100

    Start a dock view
    A first view in the shell, with three items in a row. RootView does not
    use it yet.

---
+++ ui/Toolkit/Sources/Shell/Dock.swift
@@ -0,0 +1,20 @@
+//
+//  Dock.swift
+//  mydistro
+//
+//  Created by Nick Hagi on 20/09/2026.
+//
+import Toolkit
+
+struct DockView: View {
+
+    var body: some View {
+        HStack {
+            Text("A1")
+            Text("A2")
+            Text("A3")
+        }
+        .padding()
+        .background(Color.green)
+    }
+}
